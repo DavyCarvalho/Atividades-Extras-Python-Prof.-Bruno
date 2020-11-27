c80cedcfb6380ac791b8121c694c33637c124302
Davy <davycarvalho2017@hotmail.com>
2020-11-27 14:37:16 -0300

    atualizando

--- exercicio04.py
+++ exercicio04.py
@@ -2,6 +2,7 @@
 # Faça um programa que gera uma lista dos números primos existentes entre 1 e 
 # um número inteiro informado pelo usuário. 
 
+# NOT DONE YET !!!
     
 num = int(input("\nDigite um número: "))
 
